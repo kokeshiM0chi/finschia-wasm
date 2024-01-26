771ac1b458e220246a0d58483e295e6138e9f187
kokeshiM0chi <frogandfrog009@gmail.com>
2024-01-26 15:17:47 +0900

    fix: modify check_result

--- scripts/integration_test_contracts.sh
+++ scripts/integration_test_contracts.sh
@@ -1,65 +1,83 @@
-#!/bin/sh
+#!/bin/bash
 
 ## parameters
-CONTRACT_ADDRESS="link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8"
-FROM_ACCOUNT="link146asaycmtydq45kxc8evntqfgepagygelel00h"
-URL="http://localhost:26658"
-CODE_ID=1
+CONTRACT_ADDRESS='link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8'
+FROM_ACCOUNT='link146asaycmtydq45kxc8evntqfgepagygelel00h'
+URL='http://localhost:26658'
 
-# Wait until the height of the block is greater than or equal to 1.
-timeout 60 bash -c "until fnsad query block 1 --node ${URL} --chain-id simd-testing --keyring-backend test; do sleep 0.5 done"
-
+## Wait until the height of the block is greater than or equal to 1.
+timeout 60 bash -c "until fnsad query block 1 --node ${URL} --chain-id simd-testing > /dev/null; do sleep 0.5; done"
 # If the timeout fails, the process will be terminated abnormally.
 exitstatus=$?
 if [[ $exitstatus -ne 0 ]]; then
     echo "timeout failed"
     exit 1
 fi
-
 # send transaction to node
 cur_path=`pwd`
 
 artifacts_path="artifacts"
-
 # This is a function that checks if the result is as expected.
 check_result() {
-    if [[ "$result" == *"failed"* ]]; then
-      exit 1
-    fi
+    if [[ "$1" != "${expected_key}" || "$2" != "${expected_val}" ]]; then echo "err with $3"; exit 1; fi
 }
 
 #*** store collection contract ***
 result=$(fnsad tx wasm store ${artifacts_path}/collection.wasm --node ${URL} --from ${FROM_ACCOUNT} --chain-id simd-testing --keyring-backend test --gas 10000000 -b block -o json -y)
 
 ## confirm a result of `store`
-check_result
+key=$(echo "${result}" | jq '.logs[] | .events' | jq '.[0]' | jq '.attributes[2]' | jq '.key')
+value=$(echo "${result}" | jq '.logs[] | .events' | jq '.[0]' | jq '.attributes[2]' | jq '.value')
+expected_key='"module"'
+expected_val='"wasm"'
+check_result ${key} ${value} "expected"
 
 #*** instantiate collection contract ***
-result=$(fnsad tx wasm instantiate ${CODE_ID} '{"name":"collection_name","uri":"collection_uri","meta":"collection_meta", "owner":${CONTRACT_ADDRESS}}' --label collection1 --admin ${FROM_ACCOUNT}  --from ${FROM_ACCOUNT} --node ${URL} --chain-id simd-testing --keyring-backend test -b block -o json -y)
+result=$(fnsad tx wasm instantiate 1 '{"name":"collection_name","uri":"collection_uri","meta":"collection_meta", "owner":"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8"}' --label collection1 --admin link146asaycmtydq45kxc8evntqfgepagygelel00h  --from link146asaycmtydq45kxc8evntqfgepagygelel00h --node http://localhost:26658 --chain-id simd-testing --keyring-backend test -b block -o json -y)
 
 ## confirm a result of `instantiate`
-check_result
+key=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[1]' | jq '.key')
+value=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[1]' | jq '.value')
+expected_key='"code_id"'
+expected_val='"1"'
+check_result ${key} ${value} "expected"
 
 #*** issue_nft collection contract ***
-result=$(fnsad tx wasm execute ${CONTRACT_ADDRESS} '{"issue_nft":{"name":"nft1_name","meta":"nft1_meta","owner":${CONTRACT_ADDRESS}}}' --from ${FROM_ACCOUNT} --node ${URL} --chain-id simd-testing --keyring-backend test --gas 10000000 -b block -o json -y)
+result=$(fnsad tx wasm execute ${CONTRACT_ADDRESS} '{"issue_nft":{"name":"nft1_name","meta":"nft1_meta","owner":"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8"}}' --from ${FROM_ACCOUNT} --node ${URL} --chain-id simd-testing --keyring-backend test --gas 10000000 -b block -o json -y)
 
 ## confirm a result of `issue_nft`
-check_result
+key=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[0]' | jq '.key')
+value=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[0]' | jq '.value')
+expected_key='"_contract_address"'
+expected_val='"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8"'
+check_result ${key} ${value} "expected"
 
 #*** mint_nft collection contract ***
-result=$(fnsad tx wasm execute ${CONTRACT_ADDRESS} '{"mint_nft":{"from":${CONTRACT_ADDRESS},"to":${CONTRACT_ADDRESS},"params":[{"token_type":"10000001","name":"nft1_name1","meta":"nft1_meta1"},{"token_type":"10000001","name":"nft1_name2","meta":"nft1_meta2"}]}}' --from ${FROM_ACCOUNT} --node ${URL} --chain-id simd-testing --keyring-backend test --gas 10000000 -b block -o json -y)
+result=$(fnsad tx wasm execute ${CONTRACT_ADDRESS} '{"mint_nft":{"from":"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8","to":"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8","params":[{"token_type":"10000001","name":"nft1_name1","meta":"nft1_meta1"},{"token_type":"10000001","name":"nft1_name2","meta":"nft1_meta2"}]}}' --from ${FROM_ACCOUNT} --node ${URL} --chain-id simd-testing --keyring-backend test --gas 10000000 -b block -o json -y)
 
 ## confirm a result of `mint_nft`
-check_result
+key=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[0]' | jq '.key')
+value=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[0]' | jq '.value')
+expected_key='"_contract_address"'
+expected_val='"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8"'
+check_result ${key} ${value} "expected"
 
 #*** send_nft collection contract ***
-result=$(fnsad tx wasm execute ${CONTRACT_ADDRESS} '{"send_nft":{"from":${CONTRACT_ADDRESS},"to":${CONTRACT_ADDRESS},"token_ids":["1000000100000001"]}}' --from ${FROM_ACCOUNT} --keyring-backend test --node ${URL} --chain-id simd-testing --gas 10000000 -b block -o json -y)
+result=$(fnsad tx wasm execute ${CONTRACT_ADDRESS} '{"send_nft":{"from":"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8","to":"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8","token_ids":["1000000100000001"]}}' --from ${FROM_ACCOUNT} --keyring-backend test --node ${URL} --chain-id simd-testing --gas 10000000 -b block -o json -y)
 
 ## confirm a result of `send_nft`
-check_result
+key=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[0]' | jq '.key')
+value=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[0]' | jq '.value')
+expected_key='"_contract_address"'
+expected_val='"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8"'
+check_result ${key} ${value} "expected"
 
 #*** burn_nft collection contract ***
-result=$(fnsad tx wasm execute ${CONTRACT_ADDRESS} '{"burn_nft":{"from":${CONTRACT_ADDRESS},"token_ids":["1000000100000001"]}}' --from ${FROM_ACCOUNT} --keyring-backend test --node ${URL} --chain-id simd-testing --gas 10000000 -b block -o json -y)
+result=$(fnsad tx wasm execute ${CONTRACT_ADDRESS} '{"burn_nft":{"from":"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8","token_ids":["1000000100000001"]}}' --from ${FROM_ACCOUNT} --keyring-backend test --node ${URL} --chain-id simd-testing --gas 10000000 -b block -o json -y)
 
 ## confirm a result of `burn_nft`
-check_result
+key=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[0]' | jq '.key')
+value=$(echo "${result}" | jq '.logs[] | .events[0]' | jq '.attributes[0]' | jq '.value')
+expected_key='"_contract_address"'
+expected_val='"link14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9sgf2vn8"'
+check_result ${key} ${value} "expected"
