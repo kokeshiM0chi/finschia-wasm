eedd3e877fa5b72cec6739395910afc8c5cdb65a
kokeshiM0chi <frogandfrog009@gmail.com>
2024-02-02 11:56:34 +0900

    tmp

--- packages/proto-build/src/git.rs
+++ packages/proto-build/src/git.rs
@@ -46,6 +46,6 @@ pub fn checkout_submodule(dir: &str, rev: &str) {
     let full_path   = |p: &str| PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(p);
     let mut cmd_git = process::Command::new("git");
     let full_path_dir = full_path(dir).to_str().expect("error");
-    err_msg = format!("Checkout Error; Path: {}", full_path_dir);
-    cmd_git.arg("-C").arg(full_path_dir).arg("checkout").arg(rev).status().expect(err_msg);
+    let err_msg = format!("Checkout Error; Path: {}", full_path_dir);
+    cmd_git.arg("-C").arg(full_path_dir).arg("checkout").arg(rev).status().expect(&err_msg);
 }
