6af5e6b86032e6e751545ac410062a9efb91e654
kokeshiM0chi <frogandfrog009@gmail.com>
2024-02-02 11:52:58 +0900

    fix: fix Option<> to &str for git assined val

--- packages/proto-build/src/git.rs
+++ packages/proto-build/src/git.rs
@@ -45,5 +45,7 @@ pub fn update_submodule(dir: &str, rev: &str) {
 pub fn checkout_submodule(dir: &str, rev: &str) {
     let full_path   = |p: &str| PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(p);
     let mut cmd_git = process::Command::new("git");
-    cmd_git.arg("-C").arg(full_path(dir)).arg("checkout").arg(rev).status().expect("error");
+    let full_path_dir = full_path(dir).to_str().expect("error");
+    err_msg = format!("Checkout Error; Path: {}", full_path_dir);
+    cmd_git.arg("-C").arg(full_path_dir).arg("checkout").arg(rev).status().expect(err_msg);
 }
