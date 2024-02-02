3b59831bf1bcc3fd7736c36775f82bd4c07fbb34
kokeshiM0chi <frogandfrog009@gmail.com>
2024-02-02 12:53:42 +0900

    tmp

--- packages/proto-build/src/git.rs
+++ packages/proto-build/src/git.rs
@@ -47,6 +47,5 @@ pub fn checkout_submodule(dir: &str, rev: &str) {
     let mut cmd_git = process::Command::new("git");
     let full_path_dir = full_path(dir).to_string_lossy().to_string();
     let err_msg = format!("Checkout Error; Path: {}", &full_path_dir);
-    cmd_git.arg("-C").arg(&full_path_dir.clone()).arg("fetch").status().expect(&err_msg);
     cmd_git.arg("-C").arg(&full_path_dir.clone()).arg("checkout").arg(rev).status().expect(&err_msg);
 }
