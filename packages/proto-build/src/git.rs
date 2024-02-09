2f87cba3caaf492316c76da321a0bdc714fa5bac
kokeshiM0chi <52264064+kokeshiM0chi@users.noreply.github.com>
2024-02-09 15:40:51 +0900

    Auto build and create bumpup pr (#9)
    * fix: delete a file for osmosis
    
    * feature: add command for `git checkout the tag` and call it
    
    * feat: add rust action cache
    
    * feat: add action to bump up proto files, build these and create this bump up pr automatically
    
    * feat: add schedule trigger once a day
    
    * feat: add linter and tests for pull request
    
    * fix: separate the job to check whether the finschia tag is new and the job to update using the tag
    
    * fix: upgrade the versions of actions/cache and actions/checkout
    
    from actions/cache@v2 to actions/cache@v3
    from actions/chackout@v3 to actions/checkout@v4
    
    * fix: add line at end of file line
    
    * fix: upgrade from actions/checkout@v3 to actions/checkout@v4
    
    * fix: Change to perform "if: needs.check-new-finschia-tag.outputs.NEW_TAG" in job instead of checking "if: env.NEW_TAG" in each step
    
    * fix: swap the order of "job dependency definition" and "job dependency source value condition check"
    
    * fix: add the tag "build:" to the commit message of the auto bumpup pr
    
    * fix: delete unnecessary "| cat" when the env file is overwritten
    
    * fix: correct omission (delete unnecessary "| cat" when the env file is overwritten)

--- packages/proto-build/src/git.rs
+++ packages/proto-build/src/git.rs
@@ -1,43 +1,16 @@
-use log::{info, warn};
-use std::ffi::OsStr;
 use std::path::PathBuf;
 use std::process;
 
-fn run_git(args: impl IntoIterator<Item = impl AsRef<OsStr>>) -> Result<(), String> {
-    let stdout = process::Stdio::inherit();
-
-    let exit_status = process::Command::new("git")
-        .args(args)
-        .stdout(stdout)
-        .status()
-        .expect("git exit status missing");
-
-    if !exit_status.success() {
-        return Err(format!(
-            "git exited with error code: {:?}",
-            exit_status.code()
-        ));
-    }
-
-    Ok(())
-}
-
-pub fn update_submodule(dir: &str, rev: &str) {
+pub fn checkout_submodule(dir: &str, rev: &str) {
     let full_path = |p: &str| PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(p);
-
-    info!("Updating {} submodule...", dir);
-
-    // switch to the given revision
-    run_git(["-C", full_path(dir).to_str().unwrap(), "checkout", rev]).expect("failed to checkout");
-
-    // pull the latest changes
-    match run_git(["-C", full_path(dir).to_str().unwrap(), "pull"]) {
-        Ok(_) => info!("Updated {} submodule to revision {}", dir, rev),
-        Err(_) => warn!(
-            "Failed to update {} with revision {}. This might be caused by revision is a tag",
-            dir, rev
-        ),
-    };
-
-    // run_git(["submodule", "update", "--init"]).expect("failed to update submodules");
+    let mut cmd_git = process::Command::new("git");
+    let full_path_dir = full_path(dir).to_string_lossy().to_string();
+    let err_msg = format!("Checkout Error; Path: {}", &full_path_dir);
+    cmd_git
+        .arg("-C")
+        .arg(&full_path_dir.clone())
+        .arg("checkout")
+        .arg(rev)
+        .status()
+        .expect(&err_msg);
 }
